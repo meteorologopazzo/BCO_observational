9a1aabd3bcd46a9fea0445db654a3173f15451cb
Alessandro Storer <m301248@levante6.lvt.dkrz.de>
2026-07-16 16:26:23 +0200

    during parachute conference

--- mass_fluxes_profiles/yk22_mass_fluxes.py
+++ mass_fluxes_profiles/yk22_mass_fluxes.py
@@ -87,7 +87,7 @@ def temp_from_h(theta, p, h_cd, Lv, cp, sat_frac):
     # - cp : specific heat dry air = 1004.67 J/kg K
     # - sat_frac : fraction of saturation, to test when different from 1.
 
-    es = 6.1121 * np.exp(17.502 * ( theta -273.15) / (240.97 + ( theta -273.15)))     # TRY HERE TO USE theta*(p/1000.)**(R/cp) - returns physically consistent values, but mass fluxes become negative :/
+    es = 6.1121 * np.exp(17.502 * ( theta*(p/1000.)**(Rd/cp) -273.15) / (240.97 + ( theta*(p/1000.)**(Rd/cp) -273.15)))     # TRY HERE TO USE theta*(p/1000.)**(R/cp) - returns physically consistent values, but mass fluxes become negative :/
     denominator = p - 0.378 * es * (1.0007 + p * 3.46e-6)
     
     return h_cd/cp - theta - sat_frac*(Lv / cp)*0.622*es*(1.0007 + p * 3.46e-6) / denominator
